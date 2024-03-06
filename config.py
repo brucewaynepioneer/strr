038966b56b75e0fba044e356c0cafa966fa9e583
brucewaynepioneer <158065590+brucewaynepioneer@users.noreply.github.com>
2024-03-07 00:21:54 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = "26482070"
+API_HASH = "28f1d67e2693e057ae4611521c555136"
+
+BOT_TOKEN = "6801894069:AAF4PHEXx9zRLAJoTY8oACPqpeenckMG8Bk"
+OWNER_ID = "6441456023"
+
+MONGO_DB_URI = "mongodb+srv://knight_rider:GODGURU12345@knight.jm59gu9.mongodb.net/?retryWrites=true&w=majority"
+MUST_JOIN = "dev_gagan"
