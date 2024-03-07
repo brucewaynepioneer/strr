eaa4fc36017a344eb82808e685595192e32f05c5
brucewaynepioneer <158065590+brucewaynepioneer@users.noreply.github.com>
2024-03-08 01:00:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 API_ID = "26482070"
 API_HASH = "28f1d67e2693e057ae4611521c555136"
 
-BOT_TOKEN = "6557432874:AAGywcrSYYwKE9o_r3foRzfVM6khPLgPMxk"
+BOT_TOKEN = "7114799677:AAHh005pi7Fsh1sd2YMafFP65DRt6jzo99s"
 OWNER_ID = "6441456023"
 
 MONGO_DB_URI = "mongodb+srv://knight_rider:GODGURU12345@knight.jm59gu9.mongodb.net/?retryWrites=true&w=majority"
